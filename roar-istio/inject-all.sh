8e7fbd2c772506337805ce2ba18a5e0cf783629d
brentlaster <Brent.Laster@sas.com>
2019-07-14 14:14:33 -0400

    refactor istio issues

--- roar-istio/inject-all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for i in `kubectl get deploy -n $1 | cut -d ' ' -f1 | grep -v NAME`
-do
-        echo injecting sidecar into $i
-        kubectl get deployment $i -n $1 -o yaml | ~/istio-1.2.0/bin/istioctl kube-inject -f - | kubectl apply -f -
-done
-
